d2820533fe888abb27a29e0aacb17123ce3e9a7e
Kokox <Kokox@DESKTOP-8L382QG>
2021-06-21 16:39:33 -0500

    Se creó el sistema de login y logout usando sesiones. Se modificó la estructura de la bd.

--- src/java/Servlets/CRUDServlet.java
+++ src/java/Servlets/CRUDServlet.java
@@ -50,15 +50,7 @@ public class CRUDServlet extends HttpServlet {
         
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */    
+    
     //OPCIONES DEL CRUD (GET)
     //----------------------
     @Override
@@ -142,15 +134,7 @@ public class CRUDServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */    
+
     //OPCIONES DEL CRUD (POST)
     //------------------------
     @Override
